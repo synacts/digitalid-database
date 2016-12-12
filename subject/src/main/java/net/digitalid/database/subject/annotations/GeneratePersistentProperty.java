9be014c9cd7a1fdbf7b1ae66588cb0047025eeb4
Kaspar Etter <kaspar.etter@digitalid.net>
2016-12-12 19:43:03 +0100

    Implemented the derivation of the externally provided type of the value type in the generated persistent properties and fixed a bug with generated non-value properties.

--- subject/src/main/java/net/digitalid/database/subject/annotations/GeneratePersistentProperty.java
+++ subject/src/main/java/net/digitalid/database/subject/annotations/GeneratePersistentProperty.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeMirror;
 
@@ -17,11 +18,14 @@ import net.digitalid.utility.circumfixes.Brackets;
 import net.digitalid.utility.circumfixes.Quotes;
 import net.digitalid.utility.collaboration.annotations.TODO;
 import net.digitalid.utility.collaboration.enumerations.Author;
+import net.digitalid.utility.conversion.converter.Converter;
 import net.digitalid.utility.generator.annotations.meta.Interceptor;
 import net.digitalid.utility.generator.information.method.MethodInformation;
 import net.digitalid.utility.generator.information.type.TypeInformation;
 import net.digitalid.utility.generator.interceptor.MethodInterceptor;
+import net.digitalid.utility.processing.logging.ProcessingLog;
 import net.digitalid.utility.processing.utility.ProcessingUtility;
+import net.digitalid.utility.processing.utility.StaticProcessingEnvironment;
 import net.digitalid.utility.processor.generator.JavaFileGenerator;
 import net.digitalid.utility.string.Strings;
 import net.digitalid.utility.validation.annotations.generation.Default;
@@ -83,13 +87,27 @@ public @interface GeneratePersistentProperty {
             final @Nonnull String surroundingType = typeInformation.getName();
             final @Nonnull List<@Nonnull ? extends TypeMirror> typeArguments = ((DeclaredType) method.getReturnType()).getTypeArguments();
             final @Nonnull String valueType = javaFileGenerator.importIfPossible(typeArguments.get(1));
-            final @Nonnull String externallyProvidedType = "Void"; // TODO: Determine this based on the converter of the value type.
+            
+            // TODO: The following code does not yet work for value converters that do not yet exist (i.e. will be generated in the same round).
+            final @Nonnull String valueConverterName = ProcessingUtility.getQualifiedName(typeArguments.get(1)) + "Converter";
+            final @Nullable TypeElement valueConverterElement = StaticProcessingEnvironment.getElementUtils().getTypeElement(valueConverterName);
+            if (valueConverterElement == null) { ProcessingLog.warning("No type element was found for $, which might be because that type will only be generated in this round.", valueConverterElement); }
+            final @Nullable DeclaredType valueConverter = valueConverterElement == null ? null : ProcessingUtility.getSupertype((DeclaredType) valueConverterElement.asType(), Converter.class);
+            
+            final @Nonnull String externallyProvidedType;
+            if (valueType.endsWith("Role")) { // TODO: This hardcoding is only temporary, of course, and should be replaced as soon as the type information can be cached and retrieved.
+                externallyProvidedType = javaFileGenerator.importIfPossible("net.digitalid.core.client.Client");
+            } else if (valueConverter != null) {
+                externallyProvidedType = javaFileGenerator.importIfPossible(valueConverter.getTypeArguments().get(1));
+            } else {
+                externallyProvidedType = "Void";
+            }
             
             final @Nonnull String converter = (valueType.equals("String") ? javaFileGenerator.importIfPossible("net.digitalid.utility.conversion.converters.StringConverter") : javaFileGenerator.importIfPossible(ProcessingUtility.getQualifiedName(typeArguments.get(1)) + "Converter")) + ".INSTANCE";
             
-            javaFileGenerator.addField("/* TODO: private */ static final @" + javaFileGenerator.importIfPossible(Nonnull.class) + " " + javaFileGenerator.importIfPossible(propertyPackage + "." + propertyType + "Table") + Brackets.inPointy(surroundingType + ", " + valueType + ", " + externallyProvidedType) + " " + upperCasePropertyName + " = " + javaFileGenerator.importIfPossible(propertyPackage + "." + propertyType + "TableBuilder") + "." + Brackets.inPointy(surroundingType + ", " + valueType + ", " + externallyProvidedType) + "withName" + Brackets.inRound(Quotes.inDouble(method.getName())) + ".withParentModule(MODULE).withValueConverter" + Brackets.inRound(converter) + ".withDefaultValue" + Brackets.inRound(method.hasAnnotation(Default.class) ? method.getAnnotation(Default.class).value() : "null") + ".build()");
+            javaFileGenerator.addField("/* TODO: private */ static final @" + javaFileGenerator.importIfPossible(Nonnull.class) + " " + javaFileGenerator.importIfPossible(propertyPackage + "." + propertyType.replace("Simple", "") + "Table") + Brackets.inPointy(surroundingType + ", " + valueType + ", " + externallyProvidedType) + " " + upperCasePropertyName + "_TABLE = " + javaFileGenerator.importIfPossible(propertyPackage + "." + propertyType.replace("Simple", "") + "TableBuilder") + "." + Brackets.inPointy(surroundingType + ", " + valueType + ", " + externallyProvidedType) + "withName" + Brackets.inRound(Quotes.inDouble(method.getName())) + ".withParentModule(MODULE).withValueConverter" + Brackets.inRound(converter) + (propertyType.contains("Value") ? ".withDefaultValue" + Brackets.inRound(method.hasAnnotation(Default.class) ? method.getAnnotation(Default.class).value() : "null") : "") + ".build()");
             
-            javaFileGenerator.addField("private final @" + javaFileGenerator.importIfPossible(Nonnull.class) + " " + javaFileGenerator.importIfPossible(propertyPackage + ".Writable" + propertyType) + Brackets.inPointy(surroundingType + ", " + valueType) + " " + method.getName() + " = " + javaFileGenerator.importIfPossible(propertyPackage + ".Writable" + propertyType + "Builder") + "." + Brackets.inPointy(surroundingType + ", " + valueType) + "withSubject(this).withTable" + Brackets.inRound(upperCasePropertyName) + ".build()");
+            javaFileGenerator.addField("private final @" + javaFileGenerator.importIfPossible(Nonnull.class) + " " + javaFileGenerator.importIfPossible(propertyPackage + ".Writable" + propertyType) + Brackets.inPointy(surroundingType + ", " + valueType) + " " + method.getName() + " = " + javaFileGenerator.importIfPossible(propertyPackage + ".Writable" + propertyType + "Builder") + "." + Brackets.inPointy(surroundingType + ", " + valueType) + "withSubject(this).withTable" + Brackets.inRound(upperCasePropertyName + "_TABLE") + ".build()");
         }
         
         @Pure
